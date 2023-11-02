441cd2a634048534b19ae459184d8f97df53ac4e
Murillo-EL-Navajas <xabiermuro1612@gmail.com>
2023-11-02 14:08:37 +0100

    activity buscador

---
+++ app/src/main/java/com/example/proyectoaplicacionlenguajesignos/activities/ActivityBuscador.java
@@ -0,0 +1,16 @@
+package com.example.proyectoaplicacionlenguajesignos.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.proyectoaplicacionlenguajesignos.R;
+
+public class ActivityBuscador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buscador);
+    }
+}
